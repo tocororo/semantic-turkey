dcbb572b41b7d23247f7b2b0014afc8cf79c7fe2
stellato@info.uniroma2.it <stellato@info.uniroma2.it@e23e5b5a-e442-0410-a6a2-63c1b3e16cff>
2009-03-09 18:45:55 +0000

    Removed file/folder

---  semanticturkey --username stellato@info.uniroma2.it/SemanticTurkeyUI/defaults/preferences/SemanticTurkey.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.semturkey.annotprops.langs", "de,en,es,fr,it,nl,pt,ru");
-pref("extensions.semturkey.annotprops.defaultlang", "en");
-pref("extensions.semturkey.extpt.annotateList", "bookmarking");
-pref("extensions.semturkey.extpt.annotate", "bookmarking");
