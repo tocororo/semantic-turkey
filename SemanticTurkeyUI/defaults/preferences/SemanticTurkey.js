063c04a3df0692459d033ef26431acd49b013f90
stellato@info.uniroma2.it <stellato@info.uniroma2.it@e23e5b5a-e442-0410-a6a2-63c1b3e16cff>
2009-03-12 16:27:02 +0000

    0.6.7

--- SemanticTurkeyUI/defaults/preferences/SemanticTurkey.js
+++ SemanticTurkeyUI/defaults/preferences/SemanticTurkey.js
@@ -1,4 +1,4 @@
 pref("extensions.semturkey.annotprops.langs", "de,en,es,fr,it,nl,pt,ru");
 pref("extensions.semturkey.annotprops.defaultlang", "en");
-//pref("extensions.semturkey.extpt.annotateList", "bookmarking");
+//pref("extensions.semturkey.extpt.annotateList", "bookmarking")  //this is dynamically resolved from the Annotation component, so it is no more necessary
 pref("extensions.semturkey.extpt.annotate", "bookmarking");
