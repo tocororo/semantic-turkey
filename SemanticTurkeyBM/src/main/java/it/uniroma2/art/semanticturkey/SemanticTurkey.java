a48b862102a4e7e635656f7bbf054309a3632d24
stellato@info.uniroma2.it <stellato@info.uniroma2.it@e23e5b5a-e442-0410-a6a2-63c1b3e16cff>
2011-09-14 15:00:40 +0000

    just updated the ChangeLog.txt file and the versionNumber in SemanticTurkey.java

--- SemanticTurkeyBM/src/main/java/it/uniroma2/art/semanticturkey/SemanticTurkey.java
+++ SemanticTurkeyBM/src/main/java/it/uniroma2/art/semanticturkey/SemanticTurkey.java
@@ -57,7 +57,7 @@ import org.slf4j.LoggerFactory;
  * 
  */
 public class SemanticTurkey {
-	public final static VersionNumber versionNumber = new VersionNumber(0, 7, 2);
+	public final static VersionNumber versionNumber = new VersionNumber(0, 7, 3);
 	static protected HttpServer s_httpServer;
 	protected static Logger logger = LoggerFactory.getLogger(SemanticTurkey.class);
 	private final static int port = 1979;
