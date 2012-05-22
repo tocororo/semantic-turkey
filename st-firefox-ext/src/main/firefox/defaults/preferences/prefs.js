4b4f3683e2bbfd3606fd2744e51dd9cb98c01bb6
manuel.fiorelli@gmail.com <manuel.fiorelli@gmail.com@e23e5b5a-e442-0410-a6a2-63c1b3e16cff>
2012-05-22 19:46:29 +0000

    Release 0.8.5: SKOS(-XL) officially supported.

--- st-firefox-ext/src/main/firefox/defaults/preferences/prefs.js
+++ st-firefox-ext/src/main/firefox/defaults/preferences/prefs.js
@@ -10,7 +10,7 @@ pref("extensions.semturkey.server.path", "/semantic_turkey/resources/stserver/ST
 pref("extensions.semturkey.isDefaultSet", false);
 pref("extensions.semturkey.defaultProjectName", "null");
 pref("extensions.semturkey.defaultProjectOntType", "null");
-pref("extensions.semturkey.version", "0.7.2");
+pref("extensions.semturkey.version", "0.8.5");
 pref("extensions.semturkey.hints.defaultProject", true);
 pref("extensions.semturkey.skos.humanReadable", false);
 pref("extensions.semturkey.skos.enabled", true);
