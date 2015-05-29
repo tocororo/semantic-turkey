b13e61e9f43cd3698649c91d57fe28ee3e636bcc
tlorenzetti <tiziano.lorenzetti@gmail.com>
2015-05-29 12:18:56 +0200

    -Fixed title of an ICV dialog -Fixed encoding problems in exporting repository

--- st-firefox-ext/src/main/firefox/chrome/semantic-turkey/content/inputOutput/exportRepository.js
+++ st-firefox-ext/src/main/firefox/chrome/semantic-turkey/content/inputOutput/exportRepository.js
@@ -2,6 +2,8 @@ if (typeof art_semanticturkey == 'undefined') var art_semanticturkey = {};
 
 Components.utils.import("resource://stmodules/Logger.jsm", art_semanticturkey);
 Components.utils.import("resource://stmodules/Alert.jsm", art_semanticturkey);
+Components.utils.import("resource://gre/modules/NetUtil.jsm");
+Components.utils.import("resource://gre/modules/FileUtils.jsm");
 Components.utils.import("resource://stservices/SERVICE_InputOutput.jsm", art_semanticturkey);
 
 buttonOkListener = function() {
@@ -15,10 +17,24 @@ buttonOkListener = function() {
 	var res = fp.show();
 	if (res == nsIFilePicker.returnOK || res == nsIFilePicker.returnReplace){
 		var pickedFile = fp.file;
-		var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].
-				createInstance(Components.interfaces.nsIFileOutputStream);
-		foStream.init(pickedFile, 0x02 | 0x08 | 0x20, 0666, 0); 
-		foStream.write(response, response.length);
-		foStream.close();
+		
+		//write response to file
+		var ostream = FileUtils.openSafeFileOutputStream(pickedFile);
+		var converter = Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
+		                createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
+		converter.charset = "UTF-8";
+		var istream = converter.convertToInputStream(response);
+		NetUtil.asyncCopy(istream, ostream);
+		alert("Repository exported succesfully");
+		
+		//Alternative valid solution
+//		var foStream = Components.classes["@mozilla.org/network/file-output-stream;1"].
+//				createInstance(Components.interfaces.nsIFileOutputStream);
+//		foStream.init(pickedFile, 0x02 | 0x08 | 0x20, 0666, 0); 
+//		var converter = Components.classes["@mozilla.org/intl/converter-output-stream;1"].
+//		         createInstance(Components.interfaces.nsIConverterOutputStream);
+//		converter.init(foStream, "UTF-8", 0, 0);
+//		converter.writeString(response);
+//		converter.close(); // this closes foStream
 	}
 };
