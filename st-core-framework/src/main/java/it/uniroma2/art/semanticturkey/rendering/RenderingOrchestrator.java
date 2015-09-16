1362582d505a83856cdd7ea6972dde6226a0679c
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2015-09-16 16:22:32 +0200

    Adopted the prefixMapping associated with the current project to render Manchester expressions in the Resource View

--- st-core-framework/src/main/java/it/uniroma2/art/semanticturkey/rendering/RenderingOrchestrator.java
+++ st-core-framework/src/main/java/it/uniroma2/art/semanticturkey/rendering/RenderingOrchestrator.java
@@ -84,7 +84,7 @@ public class RenderingOrchestrator implements RenderingEngine {
 						statements, NodeFilters.ANY, null);
 
 				if (anonCls != null) {
-					resource2rendering.put(res, anonCls.getManchExpr(true));
+					resource2rendering.put(res, anonCls.getManchExpr(project.getOntModel(), true));
 					toBeRendered = false;
 				}
 			}
