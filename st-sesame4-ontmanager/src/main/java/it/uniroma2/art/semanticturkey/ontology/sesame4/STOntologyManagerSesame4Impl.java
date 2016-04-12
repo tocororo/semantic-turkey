0f26c2b560bc033b665a85d671e148b3609cced8
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2016-04-12 16:16:32 +0200

    Upgrade to Karaf 4.0.4 and Servlet 3. Add an OntManager for Sesame4

---
+++ st-sesame4-ontmanager/src/main/java/it/uniroma2/art/semanticturkey/ontology/sesame4/STOntologyManagerSesame4Impl.java
@@ -0,0 +1,60 @@
+/*
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.1 (the "License");  you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * http//www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is ST OntologyManager - Sesame Implementation.
+ *
+ * The Initial Developer of the Original Code is University of Roma Tor Vergata.
+ * Portions created by University of Roma Tor Vergata are Copyright (C) 2008.
+ * All Rights Reserved.
+ *
+ * The ART Ontology API - Sesame Implementation were developed by the Artificial Intelligence Research Group
+ * (art.uniroma2.it) at the University of Roma Tor Vergata
+ * Current information about the ST OntologyManager - Sesame Implementation can be obtained at 
+ * http//semanticturkey.uniroma2.it
+ *
+ */
+
+
+package it.uniroma2.art.semanticturkey.ontology.sesame4;
+
+import it.uniroma2.art.owlart.models.RDFModel;
+import it.uniroma2.art.owlart.sesame4impl.factory.ARTModelFactorySesame4Impl;
+import it.uniroma2.art.owlart.sesame4impl.vocabulary.SESAME;
+import it.uniroma2.art.semanticturkey.ontology.STOntologyManager;
+import it.uniroma2.art.semanticturkey.project.Project;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Armando Stellato
+ * @author Andrea Turbati
+ * 
+ */
+public class STOntologyManagerSesame4Impl<MODELTYPE extends RDFModel> extends STOntologyManager<MODELTYPE> {
+
+	protected static Logger logger = LoggerFactory.getLogger(STOntologyManagerSesame4Impl.class);
+
+	STOntologyManagerSesame4Impl(Project<MODELTYPE> project) {
+		super(project, new ARTModelFactorySesame4Impl());
+	}
+
+
+	@Override
+	public String getId() {
+		return "it.uniroma2.art.semanticturkey.ontology.sesame2.STOntologyManagerSesame4Impl";
+	}
+
+	@Override
+	protected void declareSupportOntologies() {
+		declareSupportOntology(model.createURIResource(SESAME.NAMESPACE), false, true);
+	}
+
+}
