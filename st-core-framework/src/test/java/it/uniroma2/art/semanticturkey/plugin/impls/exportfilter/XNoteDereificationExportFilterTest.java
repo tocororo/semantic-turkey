ffbbfe424b6b0009bc9a975a563805e85bf9ce6e
turbati@info.uniroma2.it <turbati@info.uniroma2.it>
2017-01-26 15:08:00 +0100

    Implemented ST-415 - Basic Exporters (added the part relative to the de-reification of skos:note and its sub properties)

--- st-core-framework/src/test/java/it/uniroma2/art/semanticturkey/plugin/impls/exportfilter/XNoteDereificationExportFilterTest.java
+++ st-core-framework/src/test/java/it/uniroma2/art/semanticturkey/plugin/impls/exportfilter/XNoteDereificationExportFilterTest.java
@@ -8,5 +8,35 @@ public class XNoteDereificationExportFilterTest extends AbstractExportFilterTest
 	public void test1() throws Exception {
 		executeTest();
 	}
+	
+	@Test
+	public void test2() throws Exception {
+		executeTest();
+	}
+	
+	@Test
+	public void test3() throws Exception {
+		executeTest();
+	}
+	
+	@Test
+	public void test4() throws Exception {
+		executeTest();
+	}
+	
+	@Test
+	public void test5() throws Exception {
+		executeTest();
+	}
+	
+	@Test
+	public void test6() throws Exception {
+		executeTest();
+	}
+	
+	@Override
+	protected boolean isPrintEnabled(String testName) {
+		return false;
+	}
 
 }
