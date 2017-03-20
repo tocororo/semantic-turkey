0f9e6350040b4673c7af6806e0ee485b27be6e9f
turbati@info.uniroma2.it <turbati@info.uniroma2.it>
2017-03-20 15:32:42 +0100

    added the parameter reifyNotes to SKOStoSKOSXL and flattenNotes to SKOSXLtoSKOS, as requested in ST-431

--- st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/Refactor2.java
+++ st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/Refactor2.java
@@ -46,7 +46,7 @@ public class Refactor2 extends STServiceAdapter  {
 	 */
 	@STServiceOperation
 	@Write
-	public void SKOStoSKOSXL() throws URIGenerationException{
+	public void SKOStoSKOSXL(boolean reifyNotes) throws URIGenerationException{
 		logger.info("request to refactor SKOS data to SKOSXL");
 		
 		List<ConceptLabelValueGraph> conceptLabelValueGraphList = new ArrayList<ConceptLabelValueGraph>();
@@ -58,6 +58,7 @@ public class Refactor2 extends STServiceAdapter  {
 		//first of all, take the SKOS data you need to refactor
 		
 		//get all the info regarding the labels(pref/alt/hidden)
+		// @formatter:off
 		String selectQuery = 
 				"PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n" + 
 				"PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> \n" + 
@@ -70,6 +71,7 @@ public class Refactor2 extends STServiceAdapter  {
 				"FILTER(?propLabel = skos:prefLabel || ?propLabel = skos:altLabel || ?propLabel = skos:hiddenLabel) \n" +
 				"}" +
 				"}";
+		// @formatter:on
 		
 		TupleQuery select = getManagedConnection().prepareTupleQuery(selectQuery);
 		
@@ -94,46 +96,50 @@ public class Refactor2 extends STServiceAdapter  {
 		}
 		tupleQueryResult.close();
 		
-		//get all the info regarding the notes (and its subproperties)
-		selectQuery = 
-				"PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n" + 
-				"PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> \n" + 
-				"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" + 
-				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" + 
-				
-				"SELECT ?concept ?propNote ?value ?graph\n" +
-				"WHERE {\n" +
-				"?propNote rdfs:subPropertyOf* skos:note . \n" + 
-				"GRAPH "+workingGraphString+" {?concept ?propNote ?value . }" +
-				"}";
-		select = getManagedConnection().prepareTupleQuery(selectQuery);
-		
-		dataset = new SimpleDataset();
-		
-		contextList = QueryResults.asList(getManagedConnection().getContextIDs());
-		for(Resource iri : contextList){
-			if(iri instanceof IRI){
-				//add the defaults graphs (used outside the GRAPH sections of the query)
-				dataset.addDefaultGraph((IRI) iri);
+		if(reifyNotes){
+			//get all the info regarding the notes (and its subproperties)
+			// @formatter:off
+			selectQuery = 
+					"PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n" + 
+					"PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> \n" + 
+					"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" + 
+					"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" + 
+					
+					"SELECT ?concept ?propNote ?value ?graph\n" +
+					"WHERE {\n" +
+					"?propNote rdfs:subPropertyOf* skos:note . \n" + 
+					"GRAPH "+workingGraphString+" {?concept ?propNote ?value . }" +
+					"}";
+			// @formatter:on
+			select = getManagedConnection().prepareTupleQuery(selectQuery);
+			
+			dataset = new SimpleDataset();
+			
+			contextList = QueryResults.asList(getManagedConnection().getContextIDs());
+			for(Resource iri : contextList){
+				if(iri instanceof IRI){
+					//add the defaults graphs (used outside the GRAPH sections of the query)
+					dataset.addDefaultGraph((IRI) iri);
+				}
 			}
+			//add the named graphs (used in the GRAPH sections of the query: WHERE and INSERT
+			dataset.addNamedGraph(workingGraph);
+			select.setDataset(dataset);
+			
+			//execute the query
+			tupleQueryResult = select.evaluate();
+			while(tupleQueryResult.hasNext()){
+				BindingSet bindingSet = tupleQueryResult.next();
+				IRI concept = (IRI) bindingSet.getBinding("concept").getValue();
+				IRI noteType = (IRI) bindingSet.getBinding("propNote").getValue();
+				Literal value = (Literal) bindingSet.getBinding("value").getValue();
+				//IRI graph = (IRI) bindingSet.getBinding("graph").getValue();
+				conceptNoteValueGraphList.add(new ConceptNoteValueGraph(concept, noteType, value, workingGraph));
+			}
+			tupleQueryResult.close();
 		}
-		//add the named graphs (used in the GRAPH sections of the query: WHERE and INSERT
-		dataset.addNamedGraph(workingGraph);
-		select.setDataset(dataset);
 		
-		//execute the query
-		tupleQueryResult = select.evaluate();
-		while(tupleQueryResult.hasNext()){
-			BindingSet bindingSet = tupleQueryResult.next();
-			IRI concept = (IRI) bindingSet.getBinding("concept").getValue();
-			IRI noteType = (IRI) bindingSet.getBinding("propNote").getValue();
-			Literal value = (Literal) bindingSet.getBinding("value").getValue();
-			//IRI graph = (IRI) bindingSet.getBinding("graph").getValue();
-			conceptNoteValueGraphList.add(new ConceptNoteValueGraph(concept, noteType, value, workingGraph));
-		}
-		tupleQueryResult.close();
-		
-		//now iterate over the two lists (labels and note) to construct the necessary SKOSXL construct
+		//now iterate over the two lists (labels and note if reifyNotes is set to true) to construct the necessary SKOSXL construct
 		String updateQuery;
 		//first the label list
 		for(ConceptLabelValueGraph conceptLabelValueGraph : conceptLabelValueGraphList){
@@ -163,6 +169,7 @@ public class Refactor2 extends STServiceAdapter  {
 			String valueString = SPARQLHelp.toSPARQL(value);
 					
 			String newIRIForLabelString = SPARQLHelp.toSPARQL(newIRIForLabel);//"<"+newIRIForLabel.stringValue()+">";
+			// @formatter:off
 			updateQuery =
 					"DELETE DATA {\n" +
 					"GRAPH "+graphString+" {"+conceptString+" "+labelTypeString+" "+valueString+" }" +
@@ -174,6 +181,7 @@ public class Refactor2 extends STServiceAdapter  {
 					newIRIForLabelString+" <"+SKOSXL.LITERAL_FORM.stringValue()+"> "+valueString+" . \n" +
 					"}\n" +
 					"}";
+			// @formatter:on
 			Update update = getManagedConnection().prepareUpdate(updateQuery);
 			
 			dataset = new SimpleDataset();
@@ -187,45 +195,49 @@ public class Refactor2 extends STServiceAdapter  {
 			
 		}
 		
-		//now the notes list
-		for(ConceptNoteValueGraph conceptNoteValueGraph : conceptNoteValueGraphList){
-			IRI concept = conceptNoteValueGraph.getConcept();
-			IRI noteType = conceptNoteValueGraph.getNoteType();
-			Literal value = conceptNoteValueGraph.getValue();
-			IRI graph = conceptNoteValueGraph.getGraph();
-			
-			Map<String, Value> valueMapping = new HashMap<String, Value>();
-			valueMapping.put(URIGenerator.Parameters.lexicalForm, value);
-			valueMapping.put(URIGenerator.Parameters.lexicalizedResource, concept);
-			valueMapping.put(URIGenerator.Parameters.type, noteType);
-			IRI newIRIForNote = generateIRI(URIGenerator.Roles.xNote, valueMapping );
-			//now add the new xNote and remove the old data regarding SKOS
-			String graphString = SPARQLHelp.toSPARQL(graph); 
-			String conceptString = SPARQLHelp.toSPARQL(concept);
-			String noteTypeString = SPARQLHelp.toSPARQL(noteType);
-			String valueString = SPARQLHelp.toSPARQL(value); 
-					
-			String newIRIForNoteString = SPARQLHelp.toSPARQL(newIRIForNote); 
-			updateQuery =
-					"DELETE DATA {\n" +
-					"GRAPH "+graphString+" {"+conceptString+" "+noteTypeString+" "+valueString+" }" +
-					"}; \n" +
-					"INSERT DATA {\n" +
-					"GRAPH "+graphString+" { \n" +
-					conceptString+" "+noteTypeString+" "+newIRIForNoteString+" .\n" +
-					newIRIForNoteString+" <"+RDF.VALUE.stringValue()+"> "+valueString+" . \n" +
-					"}\n" +
-					"}";
-			Update update = getManagedConnection().prepareUpdate(updateQuery);
-			
-			dataset = new SimpleDataset();
-			
-			contextList = QueryResults.asList(getManagedConnection().getContextIDs());
-			//add the named graphs (used in the GRAPH sections of the query: WHERE and INSERT
-			dataset.addNamedGraph(workingGraph);
-			update.setDataset(dataset);
-			//execute the UPDATE
-			update.execute();
+		if(reifyNotes){
+			//now the notes list
+			for(ConceptNoteValueGraph conceptNoteValueGraph : conceptNoteValueGraphList){
+				IRI concept = conceptNoteValueGraph.getConcept();
+				IRI noteType = conceptNoteValueGraph.getNoteType();
+				Literal value = conceptNoteValueGraph.getValue();
+				IRI graph = conceptNoteValueGraph.getGraph();
+				
+				Map<String, Value> valueMapping = new HashMap<String, Value>();
+				valueMapping.put(URIGenerator.Parameters.lexicalForm, value);
+				valueMapping.put(URIGenerator.Parameters.lexicalizedResource, concept);
+				valueMapping.put(URIGenerator.Parameters.type, noteType);
+				IRI newIRIForNote = generateIRI(URIGenerator.Roles.xNote, valueMapping );
+				//now add the new xNote and remove the old data regarding SKOS
+				String graphString = SPARQLHelp.toSPARQL(graph); 
+				String conceptString = SPARQLHelp.toSPARQL(concept);
+				String noteTypeString = SPARQLHelp.toSPARQL(noteType);
+				String valueString = SPARQLHelp.toSPARQL(value); 
+						
+				String newIRIForNoteString = SPARQLHelp.toSPARQL(newIRIForNote); 
+				// @formatter:off
+				updateQuery =
+						"DELETE DATA {\n" +
+						"GRAPH "+graphString+" {"+conceptString+" "+noteTypeString+" "+valueString+" }" +
+						"}; \n" +
+						"INSERT DATA {\n" +
+						"GRAPH "+graphString+" { \n" +
+						conceptString+" "+noteTypeString+" "+newIRIForNoteString+" .\n" +
+						newIRIForNoteString+" <"+RDF.VALUE.stringValue()+"> "+valueString+" . \n" +
+						"}\n" +
+						"}";
+				// @formatter:on
+				Update update = getManagedConnection().prepareUpdate(updateQuery);
+				
+				dataset = new SimpleDataset();
+				
+				contextList = QueryResults.asList(getManagedConnection().getContextIDs());
+				//add the named graphs (used in the GRAPH sections of the query: WHERE and INSERT
+				dataset.addNamedGraph(workingGraph);
+				update.setDataset(dataset);
+				//execute the UPDATE
+				update.execute();
+			}
 		}
 	}
 	
@@ -235,7 +247,7 @@ public class Refactor2 extends STServiceAdapter  {
 	 */
 	@STServiceOperation
 	@Write
-	public void SKOSXLtoSKOS(){
+	public void SKOSXLtoSKOS(boolean flattenNotes){
 		logger.info("request to refactor SKOSXL data to SKOS");
 		
 		IRI workingGraph = (IRI) getWorkingGraph();
@@ -248,13 +260,14 @@ public class Refactor2 extends STServiceAdapter  {
 				"PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" + 
 				"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" + 
 				//remove the SKOSXL data
-				"DELETE { \n"+
+				"DELETE { \n";
 				//the part relative to skos:note (and its sub properties)
-				"GRAPH "+workingGraphString+" {?concept ?propNote ?reifiedNote . \n" +
-				"?reifiedNote rdf:value ?value . } \n" +
-				
+		if(flattenNotes){
+			queryUpdate += "GRAPH "+workingGraphString+" {?concept ?propNote ?reifiedNote . \n" +
+			"?reifiedNote rdf:value ?value . } \n";
+		}
 				///the related to the labels (Pref, Alt and Hidden)
-				"GRAPH "+workingGraphString+" {?genericSubj ?genericProp1 ?label . \n " +
+		queryUpdate += "GRAPH "+workingGraphString+" {?genericSubj ?genericProp1 ?label . \n " +
 				"?label ?genericProp2 ?genericObj . }\n" +
 				
 				"} \n " +
@@ -263,9 +276,11 @@ public class Refactor2 extends STServiceAdapter  {
 				"INSERT { \n" +
 				"GRAPH "+workingGraphString+"{?concept skos:prefLabel ?prefLabelLitForm . } \n " +
 				"GRAPH "+workingGraphString+"{?concept skos:altLabel ?altLabelLitForm . } \n" +
-				"GRAPH "+workingGraphString+"{?concept skos:hiddenLabel ?hiddenLabelLitForm . } \n" +
-				"GRAPH "+workingGraphString+"{?concept ?propNote ?value .} \n" +
-				"} \n" +
+				"GRAPH "+workingGraphString+"{?concept skos:hiddenLabel ?hiddenLabelLitForm . } \n";
+		if(flattenNotes){
+			queryUpdate += "GRAPH "+workingGraphString+"{?concept ?propNote ?value .} \n";
+		}
+		queryUpdate += "} \n" +
 				//get the data in SKOSXL to be transformed in SKOS
 				"WHERE {\n" +
 			
@@ -286,16 +301,18 @@ public class Refactor2 extends STServiceAdapter  {
 				"?label skosxl:literalForm ?hiddenLabelLitForm . \n" +
 				"?genericSubj ?genericProp1 ?label . \n" +
 				"?label ?genericProp2 ?genericObj . } \n" +
-				"}\n" +
+				"}\n";
 
+		if(flattenNotes){
 				//the notes (and its sub properties) part
-				"UNION \n"+
+			queryUpdate += "UNION \n"+
 				"{?propNote rdfs:subPropertyOf* skos:note . \n" + 
 				"GRAPH "+workingGraphString+"{?concept ?propNote ?reifiedNote . \n" + 
 				"?reifiedNote rdf:value ?value .} \n" +
-				"} \n" +
+				"} \n";
+		}
 				
-				"}";
+		queryUpdate += "}";
 		// @formatter:on
 		
 		Update update = getManagedConnection().prepareUpdate(queryUpdate);
