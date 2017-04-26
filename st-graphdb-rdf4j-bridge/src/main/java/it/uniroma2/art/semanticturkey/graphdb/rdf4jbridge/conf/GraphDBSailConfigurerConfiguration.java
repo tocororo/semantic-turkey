b6d4d93480465de722282a974537eb0f31ad0068
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2017-04-26 17:54:30 +0200

    Suspend attempt at integrating GraphDB Free

--- st-graphdb-rdf4j-bridge/src/main/java/it/uniroma2/art/semanticturkey/graphdb/rdf4jbridge/conf/GraphDBSailConfigurerConfiguration.java
+++
@@ -1,7 +0,0 @@
-package it.uniroma2.art.semanticturkey.graphdb.rdf4jbridge.conf;
-
-import it.uniroma2.art.semanticturkey.plugin.configuration.AbstractPluginConfiguration;
-
-public abstract class GraphDBSailConfigurerConfiguration extends AbstractPluginConfiguration {
-
-}
