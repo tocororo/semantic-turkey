07ef5e82fbf8886ac73a47c22b800d35c1092a22
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2017-05-08 19:16:40 +0200

    Remove old rendering mechanism based on the shower extension point

--- st-core-framework/src/main/java/it/uniroma2/art/semanticturkey/plugin/extpts/ShowerInterface.java
+++
@@ -1,8 +0,0 @@
-package it.uniroma2.art.semanticturkey.plugin.extpts;
-
-import it.uniroma2.art.semanticturkey.ontology.utilities.STRDFResource;
-
-public interface ShowerInterface extends STOSGIExtension {
-
-	public void apply(Object object);
-}
