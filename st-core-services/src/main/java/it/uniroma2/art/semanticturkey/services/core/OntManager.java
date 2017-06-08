33224d717833356d5d672229f11829ff9c16745b
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2017-06-08 16:44:41 +0200

    Add missing update types to the OntManager service

--- st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/OntManager.java
+++ st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/OntManager.java
@@ -5,15 +5,20 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.util.Collection;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 
 import it.uniroma2.art.semanticturkey.resources.MirroredOntologyFile;
 import it.uniroma2.art.semanticturkey.resources.OntologiesMirror;
 import it.uniroma2.art.semanticturkey.resources.Resources;
 import it.uniroma2.art.semanticturkey.services.STServiceAdapter;
+import it.uniroma2.art.semanticturkey.services.annotations.Optional;
+import it.uniroma2.art.semanticturkey.services.annotations.RequestMethod;
 import it.uniroma2.art.semanticturkey.services.annotations.STService;
 import it.uniroma2.art.semanticturkey.services.annotations.STServiceOperation;
 import it.uniroma2.art.semanticturkey.services.core.ontmanager.OntologyMirrorEntryInfo;
@@ -31,7 +36,7 @@ public class OntManager extends STServiceAdapter {
 	private static Logger logger = LoggerFactory.getLogger(OntManager.class);
 
 	public static enum UpdateType {
-		webUpdate, localUpdate
+		updateFromBaseURI, updateFromAlternativeURL, updateFromFile
 	};
 
 	/**
@@ -52,7 +57,7 @@ public class OntManager extends STServiceAdapter {
 	 * @param baseURI
 	 * @param cacheFileName
 	 */
-	@STServiceOperation
+	@STServiceOperation(method = RequestMethod.POST)
 	public void deleteOntologyMirrorEntry(String baseURI, String cacheFileName) {
 		OntologiesMirror.removeCachedOntologyEntry(baseURI);
 		File cacheFile = new File(Resources.getOntologiesMirrorDir(), cacheFileName);
@@ -60,20 +65,35 @@ public class OntManager extends STServiceAdapter {
 	}
 
 	/**
-	 * Updates an entry (and its associated physical file) from the Ontology Mirror
+	 * Updates an entry (and its associated physical file) from the Ontology Mirror. The entry can be updated
+	 * in three different ways (determined by the parameter {@code updateType}, differentiating in the source
+	 * of the updated ontology:
+	 * <ul>
+	 * <li>{@code updateFromBaseURI}: the source is retrieved from the supplied {@code baseURI}</li>
+	 * <li>{@code updateFromAlternativeURL}: the source is retrieved from the address hold by the parameter
+	 * {@code alternativeURL}</li>
+	 * <li>{@code updateFromFile}: the source has been supplied in the request body (and mapped to the
+	 * parameter {@code inputFile})</li>
+	 * </ul>
 	 * 
 	 * @param baseURI
 	 * @param cacheFileName
-	 * @throws IOException 
+	 * @throws IOException
 	 */
-	@STServiceOperation
+	@STServiceOperation(method = RequestMethod.POST)
 	public void updateOntologyMirrorEntry(UpdateType updateType, String baseURI, String mirrorFileName,
-			String location) throws IOException {
+			@Optional String alternativeURL, @Optional MultipartFile inputFile) throws IOException {
 		MirroredOntologyFile mirFile = new MirroredOntologyFile(mirrorFileName);
 
-		if (updateType == UpdateType.webUpdate) { // use first a temporary file, just in case the download brokes in
-			// the middle, then copies the temporary to the destination in the
-			// mirror
+		if (updateType == UpdateType.updateFromBaseURI || updateType == UpdateType.updateFromAlternativeURL) {
+			String location;
+
+			if (updateType == UpdateType.updateFromAlternativeURL) {
+				location = alternativeURL;
+			} else {
+				location = baseURI;
+			}
+
 			File tempFile = Files.createTempFile("updateOntMirrorEntry", "temp").toFile();
 			try {
 				Utilities.downloadRDF(new URL(location), tempFile.getAbsolutePath());
@@ -81,8 +101,9 @@ public class OntManager extends STServiceAdapter {
 			} finally {
 				tempFile.delete();
 			}
-		} else if (updateType == UpdateType.localUpdate) {
-			Utilities.copy(location, mirFile.getAbsolutePath());
+		} else if (updateType == UpdateType.updateFromFile) {
+			Objects.requireNonNull(inputFile, "Missing required input file");
+			FileUtils.copyInputStreamToFile(inputFile.getInputStream(), mirFile.getFile());
 		}
 	}
 }
