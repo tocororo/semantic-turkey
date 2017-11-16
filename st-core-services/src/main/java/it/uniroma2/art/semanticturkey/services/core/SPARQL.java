e68a088a8384d21529aa29cabd819453534c2ae5
tlorenzetti <tiziano.lorenzetti@gmail.com>
2017-11-16 15:05:01 +0100

    Added dependencies to sheet2rdf; Updated coda version to 1.5-SNAPSHOT; Other changes to minor dependencies; Implemented a service to export query result in spreadsheet formats (ST-715).

--- st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/SPARQL.java
+++ st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/SPARQL.java
@@ -1,17 +1,33 @@
 package it.uniroma2.art.semanticturkey.services.core;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Nullable;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.eclipse.rdf4j.model.IRI;
+import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.model.Value;
+import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.BooleanQuery;
 import org.eclipse.rdf4j.query.Dataset;
 import org.eclipse.rdf4j.query.GraphQuery;
+import org.eclipse.rdf4j.query.GraphQueryResult;
 import org.eclipse.rdf4j.query.Operation;
 import org.eclipse.rdf4j.query.Query;
 import org.eclipse.rdf4j.query.QueryLanguage;
@@ -23,6 +39,8 @@ import org.eclipse.rdf4j.query.impl.IteratingTupleQueryResult;
 import org.eclipse.rdf4j.query.impl.SimpleDataset;
 import org.eclipse.rdf4j.query.resultio.sparqljson.SPARQLResultsJSONWriter;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.rio.ntriples.NTriplesUtil;
+import org.jopendocument.dom.spreadsheet.SpreadSheet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -183,6 +201,114 @@ public class SPARQL extends STServiceAdapter {
 				defaultInsertGraph, defaultRemoveGraphs, preparedUpdate);
 
 		preparedUpdate.execute();
+	}
+	
+	/**
+	 * Exports the query result as spreadsheet
+	 * @param oRes
+	 * @param format xlsx for Microsoft Excel spreadsheet or ods for LibreOffice spreadsheet 
+	 * @param query
+	 * @param ql
+	 * @param includeInferred
+	 * @param bindings
+	 * @param maxExecTime
+	 * @param defaultGraphs
+	 * @param namedGraphs
+	 * @throws JsonProcessingException
+	 * @throws IOException
+	 */
+	@STServiceOperation(method = RequestMethod.POST)
+	@Read
+	@PreAuthorize("@auth.isAuthorized('rdf(sparql)', 'R')")
+	public void exportQueryResultAsSpreadsheet(HttpServletResponse oRes, String format,
+			String query, @Optional(defaultValue = "SPARQL") QueryLanguage ql,
+			@Optional(defaultValue = "true") boolean includeInferred,
+			@Optional(defaultValue = "{}") Map<String, Value> bindings,
+			@Optional(defaultValue = "0") int maxExecTime, @Optional(defaultValue = "") IRI[] defaultGraphs,
+			@Optional(defaultValue = "") IRI[] namedGraphs) throws JsonProcessingException, IOException {
+		
+		if (!format.equals("xlsx") && !format.equals("ods")) {
+			throw new IllegalArgumentException("Invalid spreadsheet format " + format 
+					+ ". Available formats are: 'xlsx' and 'ods'");
+		}
+		
+		RepositoryConnection conn = getManagedConnection();
+		Query preparedQuery = conn.prepareQuery(ql, query);
+		configureOperation(includeInferred, bindings, maxExecTime, defaultGraphs, namedGraphs, null, null,
+				preparedQuery);
+		
+		List<List<String>> table = new ArrayList<>();
+		
+		if (preparedQuery instanceof TupleQuery) {
+			TupleQueryResult result = ((TupleQuery) preparedQuery).evaluate();
+			List<String> bindingNames = result.getBindingNames();
+			table.add(bindingNames);
+			while (result.hasNext()) {
+				List<String> row = new ArrayList<>();
+				BindingSet tuple = result.next();
+				for (String b : bindingNames) {
+					row.add(NTriplesUtil.toNTriplesString(tuple.getValue(b)));
+				}
+				table.add(row);
+			}
+		} else { // must be (preparedQuery instanceof GraphQuery)
+			GraphQueryResult result = ((GraphQuery) preparedQuery).evaluate();
+			table.add(Arrays.asList("subj", "pred", "obj"));
+			while (result.hasNext()) {
+				List<String> row = new ArrayList<>();
+				Statement stmt = result.next();
+				row.add(NTriplesUtil.toNTriplesString(stmt.getSubject()));
+				row.add(NTriplesUtil.toNTriplesString(stmt.getPredicate()));
+				row.add(NTriplesUtil.toNTriplesString(stmt.getObject()));
+				table.add(row);
+			}
+		}
+	    dumpSpreadsheet(oRes, table, format);
+	}
+	
+	private void dumpSpreadsheet(HttpServletResponse oRes, List<List<String>> table, String format) throws IOException {
+		File tempServerFile = File.createTempFile("sparqlExport", "." + format);
+		
+		if (format.equals("xlsx")) {
+			Workbook wb = new XSSFWorkbook();
+			Sheet sheet = wb.createSheet();
+			for (int rowIdx = 0; rowIdx < table.size(); rowIdx++) {
+				List<String> tableRow = table.get(rowIdx);
+				Row row = sheet.createRow(rowIdx);
+				for (int colIdx = 0; colIdx < tableRow.size(); colIdx++) {
+					row.createCell(colIdx).setCellValue(table.get(rowIdx).get(colIdx));
+				}
+			}
+			try (OutputStream tempServerFileStream = new FileOutputStream(tempServerFile)) {
+				wb.write(tempServerFileStream);
+			}
+		} else { //format .ods (do not perform any check since it is already done in the calling method
+			SpreadSheet spreadSheet = SpreadSheet.create(1, table.get(0).size(), table.size());
+		    org.jopendocument.dom.spreadsheet.Sheet sheet = spreadSheet.getSheet(0);
+		    for (int rowIdx = 0; rowIdx < table.size(); rowIdx++) {
+		    	List<String> tableRow = table.get(rowIdx);
+		    	for (int colIdx = 0; colIdx < tableRow.size(); colIdx++) {
+		    		sheet.setValueAt(tableRow.get(colIdx), colIdx, rowIdx);
+		    	}
+		    }
+		    spreadSheet.saveAs(tempServerFile);
+		}
+		
+		try {
+			oRes.setHeader("Content-Disposition", "attachment; filename=export." + format);
+			if (format.equals("xlsx")) {
+				oRes.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+			} else {
+				oRes.setContentType("application/vnd.oasis.opendocument.spreadsheet");
+			}
+			oRes.setContentLength((int) tempServerFile.length());
+			try (InputStream is = new FileInputStream(tempServerFile)) {
+				IOUtils.copy(is, oRes.getOutputStream());
+			}
+			oRes.flushBuffer();
+		} finally {
+			tempServerFile.delete();
+		}
 	}
 
 	protected void configureOperation(boolean includeInferred, Map<String, Value> bindings, int maxExecTime,
