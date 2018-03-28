eea6dbf61301c4a3ee21f0308279032a8b41ecdb
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2018-03-28 17:21:26 +0200

    Update the Collaboration service to the new Extension Management framework

--- st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/Collaboration.java
+++ st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/Collaboration.java
@@ -6,31 +6,40 @@ import java.util.Map;
 import org.eclipse.rdf4j.model.IRI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import it.uniroma2.art.semanticturkey.config.InvalidConfigurationException;
 import it.uniroma2.art.semanticturkey.exceptions.InvalidProjectNameException;
 import it.uniroma2.art.semanticturkey.exceptions.ProjectAccessException;
 import it.uniroma2.art.semanticturkey.exceptions.ProjectInexistentException;
 import it.uniroma2.art.semanticturkey.exceptions.ProjectUpdateException;
 import it.uniroma2.art.semanticturkey.exceptions.ReservedPropertyUpdateException;
+import it.uniroma2.art.semanticturkey.extension.ExtensionPointManager;
+import it.uniroma2.art.semanticturkey.extension.NoSuchExtensionException;
+import it.uniroma2.art.semanticturkey.extension.NoSuchSettingsManager;
+import it.uniroma2.art.semanticturkey.extension.extpts.collaboration.CollaborationBackend;
 import it.uniroma2.art.semanticturkey.extension.extpts.collaboration.CollaborationBackendException;
 import it.uniroma2.art.semanticturkey.plugin.PluginFactory;
 import it.uniroma2.art.semanticturkey.plugin.PluginManager;
-import it.uniroma2.art.semanticturkey.plugin.extpts.CollaborationBackend;
+import it.uniroma2.art.semanticturkey.plugin.PluginSpecification;
 import it.uniroma2.art.semanticturkey.project.Project;
 import it.uniroma2.art.semanticturkey.properties.STProperties;
 import it.uniroma2.art.semanticturkey.properties.STPropertiesChecker;
 import it.uniroma2.art.semanticturkey.properties.STPropertyAccessException;
 import it.uniroma2.art.semanticturkey.properties.STPropertyUpdateException;
+import it.uniroma2.art.semanticturkey.properties.WrongPropertiesException;
+import it.uniroma2.art.semanticturkey.resources.Scope;
 import it.uniroma2.art.semanticturkey.services.STServiceAdapter;
 import it.uniroma2.art.semanticturkey.services.annotations.Optional;
 import it.uniroma2.art.semanticturkey.services.annotations.RequestMethod;
 import it.uniroma2.art.semanticturkey.services.annotations.STService;
 import it.uniroma2.art.semanticturkey.services.annotations.STServiceOperation;
+import it.uniroma2.art.semanticturkey.user.STUser;
 import it.uniroma2.art.semanticturkey.user.UsersManager;
 
 /**
@@ -42,21 +51,24 @@ public class Collaboration extends STServiceAdapter {
 
 	private static final String PROJ_PROP_BACKEND = "plugins.optional.collaboration.factoryID";
 	private static Logger logger = LoggerFactory.getLogger(Collaboration.class);
-	
-	
+
+	@Autowired
+	private ExtensionPointManager exptManager;
+
 	@STServiceOperation
 	public JsonNode getCollaborationSystemStatus(String backendId) throws STPropertyAccessException {
 		PluginFactory<?, ?, ?, ?, ?> pluginFactory = PluginManager.getPluginFactory(backendId);
 		Project project = getProject();
-		
+
 		STProperties settings = pluginFactory.getProjectSettings(project);
 		boolean settingsConfigured = STPropertiesChecker.getModelConfigurationChecker(settings).isValid();
-		
+
 		STProperties preferences = pluginFactory.getProjectPreferences(project, UsersManager.getLoggedUser());
-		boolean preferencesConfigured = STPropertiesChecker.getModelConfigurationChecker(preferences).isValid();
-		
+		boolean preferencesConfigured = STPropertiesChecker.getModelConfigurationChecker(preferences)
+				.isValid();
+
 		boolean collaborationEnabled = project.getProperty(PROJ_PROP_BACKEND) != null;
-		
+
 		JsonNodeFactory jf = JsonNodeFactory.instance;
 		ObjectNode respNode = jf.objectNode();
 		respNode.set("enabled", jf.booleanNode(collaborationEnabled));
@@ -64,78 +76,75 @@ public class Collaboration extends STServiceAdapter {
 		respNode.set("preferencesConfigured", jf.booleanNode(preferencesConfigured));
 		boolean projectLinked = collaborationEnabled ? getCollaborationBackend().isProjectLinked() : false;
 		respNode.set("linked", jf.booleanNode(projectLinked));
-		
+
 		return respNode;
 	}
-	
-	@STServiceOperation
-	public STProperties getProjectSettings(String backendId) throws STPropertyAccessException {
-		PluginFactory<?, ?, ?, ?, ?> pluginFactory = PluginManager.getPluginFactory(backendId);
-		return pluginFactory.getProjectSettings(getProject());
-	}
 
-	@STServiceOperation
-	public STProperties getProjectPreferences(String backendId) throws STPropertyAccessException {
-		PluginFactory<?, ?, ?, ?, ?> pluginFactory = PluginManager.getPluginFactory(backendId);
-
-		return pluginFactory.getProjectPreferences(getProject(), UsersManager.getLoggedUser());
-	}
-	
-	
 	@STServiceOperation(method = RequestMethod.POST)
 	public void activateCollaboratioOnProject(String backendId, Map<String, Object> projectSettings,
 			Map<String, Object> currentUserPreferences) throws STPropertyAccessException,
-			STPropertyUpdateException, ProjectUpdateException, ReservedPropertyUpdateException, 
-			InvalidProjectNameException, ProjectInexistentException, ProjectAccessException, IOException, 
-			CollaborationBackendException {
-		PluginFactory<?, ?, ?, ?, ?> pluginFactory = PluginManager.getPluginFactory(backendId);
+			STPropertyUpdateException, ProjectUpdateException, ReservedPropertyUpdateException,
+			InvalidProjectNameException, ProjectInexistentException, ProjectAccessException, IOException,
+			CollaborationBackendException, NoSuchSettingsManager, WrongPropertiesException {
 		Project project = getProject();
-		pluginFactory.storeProjectSettings(project, projectSettings);
-		pluginFactory.storeProjectPreferences(project, UsersManager.getLoggedUser(), currentUserPreferences);
+		STUser user = UsersManager.getLoggedUser();
+
+		exptManager.storeSettings(backendId, project, user, Scope.PROJECT, projectSettings);
+		exptManager.storeSettings(backendId, project, user, Scope.PROJECT_USER, currentUserPreferences);
+
 		project.setProperty(PROJ_PROP_BACKEND, backendId);
-		
-		//TODO check the parameters (url sbagliato, credenziali sbagliate, parametri progetto sbagliati)
+
+		// TODO check the parameters (url sbagliato, credenziali sbagliate, parametri progetto sbagliati)
 		getCollaborationBackend().checkPrjConfiguration();
 	}
 
 	@STServiceOperation(method = RequestMethod.POST)
-	public void addPreferenceiesForCurrentUser(String backendId, Map<String, Object> currentUserPreferences)
+	public void addPreferencesForCurrentUser(String backendId, Map<String, Object> currentUserPreferences)
 			throws STPropertyAccessException, STPropertyUpdateException, ProjectUpdateException,
-			ReservedPropertyUpdateException {
-		PluginFactory<?, ?, ?, ?, ?> pluginFactory = PluginManager.getPluginFactory(backendId);
+			ReservedPropertyUpdateException, NoSuchSettingsManager, WrongPropertiesException {
 		Project project = getProject();
-		pluginFactory.storeProjectPreferences(project, UsersManager.getLoggedUser(), currentUserPreferences);
+		STUser user = UsersManager.getLoggedUser();
+
+		exptManager.storeSettings(backendId, project, user, Scope.PROJECT_USER, currentUserPreferences);
+
 		project.setProperty(PROJ_PROP_BACKEND, backendId);
 	}
 
-	private CollaborationBackend getCollaborationBackend() {
+	private CollaborationBackend getCollaborationBackend() throws IllegalStateException {
 		String backendId = getProject().getProperty(PROJ_PROP_BACKEND);
 
-		PluginFactory<?, ?, ?, ?, ?> pluginFactory = PluginManager.getPluginFactory(backendId);
-		CollaborationBackend instance = (CollaborationBackend) pluginFactory
-				.createInstance(pluginFactory.createDefaultPluginConfiguration());
+		CollaborationBackend instance;
+		try {
+			instance = exptManager.instantiateExtension(CollaborationBackend.class,
+					new PluginSpecification(backendId, null, null, null));
+		} catch (IllegalArgumentException | NoSuchExtensionException | WrongPropertiesException
+				| STPropertyAccessException | InvalidConfigurationException e) {
+			throw new IllegalStateException(e);
+		}
 		instance.bind2project(getProject());
 		return instance;
 	}
 
 	@STServiceOperation(method = RequestMethod.POST)
-	public void createIssue(IRI resource, String summary, @Optional String description, 
-			@Optional String assignee, @Optional String issueId) throws STPropertyAccessException, IOException, CollaborationBackendException {
-		getCollaborationBackend().createIssue(resource.stringValue(), summary, description, assignee, issueId);
+	public void createIssue(IRI resource, String summary, @Optional String description,
+			@Optional String assignee, @Optional String issueId)
+			throws STPropertyAccessException, IOException, CollaborationBackendException {
+		getCollaborationBackend().createIssue(resource.stringValue(), summary, description, assignee,
+				issueId);
 	}
 
 	@STServiceOperation(method = RequestMethod.POST)
 	public void assignProject(String projectName, String projectKey, String projectId)
-			throws STPropertyAccessException, IOException, CollaborationBackendException, STPropertyUpdateException {
+			throws STPropertyAccessException, IOException, CollaborationBackendException,
+			STPropertyUpdateException {
 		getCollaborationBackend().assignProject(projectName, projectKey, projectId);
-		//TODO check the parameters (url sbagliato, credenziali sbagliate, parametri progetto sbagliati)
+		// TODO check the parameters (url sbagliato, credenziali sbagliate, parametri progetto sbagliati)
 		getCollaborationBackend().checkPrjConfiguration();
 	}
 
 	@STServiceOperation(method = RequestMethod.POST)
-	public void createProject(String projectName, String projectKey)
-			throws STPropertyAccessException, JsonProcessingException, IOException, CollaborationBackendException,
-			STPropertyUpdateException {
+	public void createProject(String projectName, String projectKey) throws STPropertyAccessException,
+			JsonProcessingException, IOException, CollaborationBackendException, STPropertyUpdateException {
 		getCollaborationBackend().createProject(projectName, projectKey);
 	}
 
@@ -150,19 +159,18 @@ public class Collaboration extends STServiceAdapter {
 			throws STPropertyAccessException, IOException, CollaborationBackendException {
 		return getCollaborationBackend().listIssuesAssignedToResource(resource);
 	}
-	
+
 	@STServiceOperation(method = RequestMethod.GET)
 	public JsonNode listIssues()
 			throws STPropertyAccessException, IOException, CollaborationBackendException {
 		return getCollaborationBackend().listIssues();
 	}
-	
+
 	@STServiceOperation(method = RequestMethod.GET)
-	public JsonNode listUsers()
-			throws STPropertyAccessException, IOException, CollaborationBackendException {
+	public JsonNode listUsers() throws STPropertyAccessException, IOException, CollaborationBackendException {
 		return getCollaborationBackend().listUsers();
 	}
-	
+
 	@STServiceOperation(method = RequestMethod.GET)
 	public JsonNode listProjects()
 			throws STPropertyAccessException, IOException, CollaborationBackendException {
