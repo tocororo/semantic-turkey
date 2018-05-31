4c4c7f1e83cb4e777d6679f4dea29ded842cff21
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2018-05-31 17:13:46 +0200

    Add members of a property chain axiom in the resource view (related to ST-1068)

--- st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/resourceview/consumers/PropertyChainStatementConsumer.java
+++ st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/resourceview/consumers/PropertyChainStatementConsumer.java
@@ -6,10 +6,12 @@ import it.uniroma2.art.semanticturkey.customform.CustomFormManager;
 import it.uniroma2.art.semanticturkey.services.core.resourceview.AbstractPropertyMatchingStatementConsumer;
 import it.uniroma2.art.semanticturkey.vocabulary.OWL2Fragment;
 
-public class PropertyChainStatementConsumer extends AbstractPropertyMatchingStatementConsumer{
+public class PropertyChainStatementConsumer extends AbstractPropertyMatchingStatementConsumer {
 
 	public PropertyChainStatementConsumer(CustomFormManager customFormManager) {
-		super(customFormManager, "subPropertyChains", Collections.singleton(OWL2Fragment.PROPERTY_CHAIN_AXIOM));
+		super(customFormManager, "subPropertyChains",
+				Collections.singleton(OWL2Fragment.PROPERTY_CHAIN_AXIOM), RootProprertiesBehavior.HIDE,
+				CollectionBehavior.ALWAYS_ASSUME_COLLECTION, SubpropertiesBehavior.INCLUDE);
 	}
 
 }
