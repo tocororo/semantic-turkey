104f0aab639c4e138915875ff9b2fed32a5246e5
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2018-12-14 16:03:06 +0100

    Remove useless package import

--- st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/OntoLexLemon.java
+++ st-core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/OntoLexLemon.java
@@ -9,7 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import javax.annotation.Nullable;
