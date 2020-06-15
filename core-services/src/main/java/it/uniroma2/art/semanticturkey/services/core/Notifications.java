2562e39c4b183172d1a2e5e0a2afde26309eb39b
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2020-06-15 09:59:21 +0200

    Add a service return available timezone ids

--- core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/Notifications.java
+++ core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/Notifications.java
@@ -1,5 +1,8 @@
 package it.uniroma2.art.semanticturkey.services.core;
 
+import java.time.ZoneId;
+import java.util.Collection;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +28,34 @@ public class Notifications extends STServiceAdapter {
 	@Autowired
 	private ResourceChangeNotificationManager notificationManager;
 
+	/**
+	 * Schedules the sending of notifications digests. The provided <code>schedule</code> combines a cron
+	 * expression (compliant with Spring flavor) and and optional time zone id. If schedule is omitted,
+	 * notifications digests are disabled.
+	 * 
+	 * @see <a href=
+	 *      "https://docs.spring.io/spring/docs/3.2.14.RELEASE/javadoc-api/org/springframework/scheduling/support/CronSequenceGenerator.html">org.springframework.scheduling.support.CronSequenceGenerator</a>
+	 * @param schedule
+	 * @throws STPropertyUpdateException
+	 */
 	@STServiceOperation(method = RequestMethod.POST)
 	public void scheduleNotificationDigest(
 			@Optional @JsonSerialized @Valid NotificationSystemSettings.CronDefinition schedule)
 			throws STPropertyUpdateException {
-		if (schedule == null)  {
+		if (schedule == null) {
 			notificationManager.disableNotificationDigest();
 		} else {
 			notificationManager.setNotificationDigestSchedule(schedule);
 		}
+	}
+
+	/**
+	 * Returns the available time zone identifiers.
+	 * 
+	 * @return
+	 */
+	@STServiceOperation
+	public Collection<String> getAvailableTimeZoneIds() {
+		return ZoneId.getAvailableZoneIds();
 	}
 }
