f79ab9842b0b0b5c72bb29bba162eb3236f60ded
Manuel Fiorelli <manuel.fiorelli@gmail.com>
2021-01-02 14:48:48 +0100

    Preliminary work on ST-1810

--- core-framework/src/main/java/it/uniroma2/art/semanticturkey/settings/project/ProjectFacets.java
+++ core-framework/src/main/java/it/uniroma2/art/semanticturkey/settings/project/ProjectFacets.java
@@ -2,11 +2,23 @@ package it.uniroma2.art.semanticturkey.settings.project;
 
 import it.uniroma2.art.semanticturkey.extension.settings.Settings;
 import it.uniroma2.art.semanticturkey.properties.RuntimeSTProperties;
+import it.uniroma2.art.semanticturkey.properties.STProperties;
+import it.uniroma2.art.semanticturkey.properties.STProperty;
 
 public class ProjectFacets extends RuntimeSTProperties implements Settings{
 
-	public ProjectFacets() {
-		super("project facets");
+	public static class MessageKeys {
+		public static final String keyBase = "it.uniroma2.art.semanticturkey.settings.project.ProjectFacets";
+
+		public static final String shortName = keyBase + ".shortName";
+		public static final String customFacets$description = keyBase + ".customFacets.description";
+		public static final String customFacets$displayName = keyBase + ".customFacets.displayName";
 	}
 
+	public ProjectFacets() {
+		super("{" + MessageKeys.shortName + "}");
+	}
+
+	@STProperty(description = "{" + MessageKeys.customFacets$description + "}", displayName = "{" + MessageKeys.customFacets$displayName + "}")
+	public STProperties customFacets;
 }
