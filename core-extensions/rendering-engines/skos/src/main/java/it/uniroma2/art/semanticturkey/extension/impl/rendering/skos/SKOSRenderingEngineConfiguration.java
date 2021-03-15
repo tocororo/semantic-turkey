eaa5488e374618716109f7c923b81e1e03710a0c
Tiziano Lorenzetti <tiziano.lorenzetti@gmail.com>
2021-03-15 16:08:45 +0100

    Fixed wrong translation keys in some RenderingEngine configurations

--- core-extensions/rendering-engines/skos/src/main/java/it/uniroma2/art/semanticturkey/extension/impl/rendering/skos/SKOSRenderingEngineConfiguration.java
+++ core-extensions/rendering-engines/skos/src/main/java/it/uniroma2/art/semanticturkey/extension/impl/rendering/skos/SKOSRenderingEngineConfiguration.java
@@ -5,7 +5,7 @@ import it.uniroma2.art.semanticturkey.extension.impl.rendering.AbstractLabelBase
 public class SKOSRenderingEngineConfiguration extends AbstractLabelBasedRenderingEngineConfiguration {
 	
 	public static class MessageKeys {
-		public static final String keyBase = "it.uniroma2.art.semanticturkey.extension.impl.rendering.skos.SKOSRenderingEnginePUSettings";
+		public static final String keyBase = "it.uniroma2.art.semanticturkey.extension.impl.rendering.skos.SKOSRenderingEngineConfiguration";
 
 		public static final String shortName = keyBase + ".shortName";
 	}
