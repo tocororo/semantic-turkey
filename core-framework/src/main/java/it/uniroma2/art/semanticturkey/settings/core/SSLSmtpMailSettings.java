d0e4c8969f504a77bd619bbde73a1e694caa6a4d
Tiziano Lorenzetti <tiziano.lorenzetti@gmail.com>
2021-04-16 12:38:26 +0200

    Added missing STProperty in SSLSmtpMailSettings; Fixed case of pt-BR language code

--- core-framework/src/main/java/it/uniroma2/art/semanticturkey/settings/core/SSLSmtpMailSettings.java
+++ core-framework/src/main/java/it/uniroma2/art/semanticturkey/settings/core/SSLSmtpMailSettings.java
@@ -1,4 +1,19 @@
 package it.uniroma2.art.semanticturkey.settings.core;
 
+import it.uniroma2.art.semanticturkey.properties.STProperty;
+
 public class SSLSmtpMailSettings {
+
+    public static class MessageKeys {
+        public static final String keyBase = "it.uniroma2.art.semanticturkey.settings.core.SSLSmtpMailSettings";
+
+        public static final String shortName = keyBase + ".shortName";
+        public static final String enable$description = keyBase + ".enable.description";
+        public static final String enable$displayName = keyBase + ".enable.displayName";
+    }
+
+    @STProperty(description = "{" + SSLSmtpMailSettings.MessageKeys.enable$description + "}", displayName = "{"
+            + SSLSmtpMailSettings.MessageKeys.enable$displayName + "}")
+    public Boolean enable;
+
 }
