9d89e2c40500d2374936cafe5d3827ab809376a1
Tiziano Lorenzetti <tiziano.lorenzetti@gmail.com>
2021-10-22 12:42:14 +0200

    Fixed minor bug in Manchester syntax rendering

--- core-framework/src/main/java/it/uniroma2/art/semanticturkey/syntax/manchester/owl2/ManchesterSyntaxUtils.java
+++ core-framework/src/main/java/it/uniroma2/art/semanticturkey/syntax/manchester/owl2/ManchesterSyntaxUtils.java
@@ -769,11 +769,11 @@ public class ManchesterSyntaxUtils {
 		} else if(iriFacet.equals(XSDFragment.MININCLUSIVE)) {
 			facet = ParserDescription.FACET_LESSEQ;
 		} else if(iriFacet.equals(XSDFragment.MINEXCLUSIVE)) {
-			facet = ParserDescription.FACET_LESS;
+			facet = ParserDescription.FACET_GREATER;
 		} else if(iriFacet.equals(XSDFragment.MAXINCLUSIVE)) {
 			facet = ParserDescription.FACET_GREATEREQ;
 		} else if(iriFacet.equals(XSDFragment.MAXEXCLUSIVE)) {
-			facet = ParserDescription.FACET_GREATER;
+			facet = ParserDescription.FACET_LESS;
 		}
 		return facet;
 	}
