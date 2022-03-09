d4c519ecf9217b4a96aa24159b4661119098c291
Andrea Turbati <aturbati@gmail.com>
2022-03-09 11:28:45 +0100

    rearrange the code to manage ST-2104 - improvements on projects to be opened at system start

--- core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/projects/ProjectStarter.java
+++ core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/projects/ProjectStarter.java
@@ -11,31 +11,47 @@ import it.uniroma2.art.semanticturkey.project.ProjectConsumer;
 import it.uniroma2.art.semanticturkey.project.ProjectManager;
 import it.uniroma2.art.semanticturkey.rbac.RBACException;
 import it.uniroma2.art.semanticturkey.rbac.RBACManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import java.util.Collection;
 
 public class ProjectStarter {
 
+    protected static Logger logger = LoggerFactory.getLogger(ProjectStarter.class);
+
     @PostConstruct
     public void openProjects() throws RBACException, ProjectAccessException {
 
         Collection<AbstractProject> abstractProjectCollection = ProjectManager.listProjects(ProjectConsumer.SYSTEM);
+
+
+        // first loop for the loadRBACProcessor
+        for (AbstractProject abstractProject : abstractProjectCollection) {
+            if (abstractProject instanceof Project) {
+                Project project = (Project) abstractProject;
+                /* initialize the RBAC processor of the project. This operation needs to be performed at
+                system startup and not when the projects are accessed so that the auth evaluation can be performed
+                also on closed project */
+                logger.info("Initializing project " + project.getName() + " ...");
+                RBACManager.loadRBACProcessor(project);
+            }
+        }
+
+        //second loop to open projects that should be opened at startup
         for (AbstractProject abstractProject : abstractProjectCollection) {
             if (abstractProject instanceof Project) {
                 Project project = (Project) abstractProject;
                 boolean openAtStart = project.isOpenAtStartupEnabled();
                 if (openAtStart) {
                     try {
+                        logger.info("Opening project " + project.getName() + " ...");
                         ProjectManager.accessProject(ProjectConsumer.SYSTEM, project.getName(), ProjectACL.AccessLevel.R, ProjectACL.LockLevel.NO);
                     } catch (InvalidProjectNameException | ProjectInexistentException | ProjectAccessException | ForbiddenProjectAccessException e) {
                         // do nothing, so just skip this project and continue opening the other projects
                     }
                 }
-                /* initialize the RBAC processor of the project. This operation needs to be performed at
-                system startup and not when the projects are accessed so that the auth evaluation can be performed
-                also on closed project */
-                RBACManager.loadRBACProcessor(project);
             }
         }
     }
