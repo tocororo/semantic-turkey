1f8257b6b7ae0bc3931bfdaa366ba4e1877fdc22
Tiziano Lorenzetti <tiziano.lorenzetti@gmail.com>
2022-10-04 14:34:16 +0200

    Improved settings and management of Custom Tree

--- core-framework/src/main/java/it/uniroma2/art/semanticturkey/settings/core/CustomTreeSettings.java
+++ core-framework/src/main/java/it/uniroma2/art/semanticturkey/settings/core/CustomTreeSettings.java
@@ -1,10 +1,13 @@
 package it.uniroma2.art.semanticturkey.settings.core;
 
 import com.sun.org.apache.xpath.internal.operations.Bool;
+import it.uniroma2.art.semanticturkey.properties.Enumeration;
 import it.uniroma2.art.semanticturkey.properties.Required;
 import it.uniroma2.art.semanticturkey.properties.STProperties;
 import it.uniroma2.art.semanticturkey.properties.STProperty;
 import org.eclipse.rdf4j.model.IRI;
+
+import java.util.List;
 
 public class CustomTreeSettings implements STProperties {
 
@@ -14,6 +17,10 @@ public class CustomTreeSettings implements STProperties {
         public static final String shortName = keyBase + ".shortName";
         public static final String enabled$description = keyBase + ".enabled.description";
         public static final String enabled$displayName = keyBase + ".enabled.displayName";
+        public static final String rootCriteria$description = keyBase + ".rootCriteria.description";
+        public static final String rootCriteria$displayName = keyBase + ".rootCriteria.displayName";
+        public static final String roots$description = keyBase + ".roots.description";
+        public static final String roots$displayName = keyBase + ".roots.displayName";
         public static final String type$description = keyBase + ".type.description";
         public static final String type$displayName = keyBase + ".type.displayName";
         public static final String includeSubtype$description = keyBase + ".includeSubtype.description";
@@ -24,8 +31,11 @@ public class CustomTreeSettings implements STProperties {
         public static final String inverseHierarchyDirection$displayName = keyBase + ".inverseHierarchyDirection.displayName";
         public static final String includeSubProp$description = keyBase + ".includeSubProp.description";
         public static final String includeSubProp$displayName = keyBase + ".includeSubProp.displayName";
-
     }
+
+    public static final String ROOT_CRITERIA_ALL = "all"; //roots are all resources of the given type without parent
+    public static final String ROOT_CRITERIA_ONLY_WITH_CHILDREN = "only_with_children"; //all resources of the given type without parent and with children
+    public static final String ROOT_CRITERIA_STATIC = "static"; //resources explicitly provided in roots setting
 
     @Override
     public String getShortName() {
@@ -35,8 +45,15 @@ public class CustomTreeSettings implements STProperties {
     @STProperty(description = "{" + MessageKeys.enabled$description+ "}", displayName = "{" + MessageKeys.enabled$displayName + "}")
     public Boolean enabled;
 
+    @STProperty(description = "{" + MessageKeys.rootCriteria$description+ "}", displayName = "{" + MessageKeys.rootCriteria$displayName + "}")
+    @Enumeration({ROOT_CRITERIA_ALL, ROOT_CRITERIA_ONLY_WITH_CHILDREN, ROOT_CRITERIA_STATIC})
+    public String rootCriteria;
+
+    @STProperty(description = "{" + MessageKeys.roots$description+ "}", displayName = "{" + MessageKeys.roots$displayName + "}")
+    public List<IRI> roots;
+
+
     @STProperty(description = "{" + MessageKeys.type$description+ "}", displayName = "{" + MessageKeys.type$displayName + "}")
-    @Required
     public IRI type;
 
     @STProperty(description = "{" + MessageKeys.includeSubtype$description+ "}", displayName = "{" + MessageKeys.includeSubtype$displayName + "}")
--- core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/CustomTrees.java
+++ core-services/src/main/java/it/uniroma2/art/semanticturkey/services/core/CustomTrees.java
@@ -2,12 +2,9 @@ package it.uniroma2.art.semanticturkey.services.core;
 
 import it.uniroma2.art.semanticturkey.properties.STPropertiesManager;
 import it.uniroma2.art.semanticturkey.properties.STPropertyAccessException;
-import it.uniroma2.art.semanticturkey.properties.STPropertyUpdateException;
 import it.uniroma2.art.semanticturkey.services.AnnotatedValue;
 import it.uniroma2.art.semanticturkey.services.STServiceAdapter;
-import it.uniroma2.art.semanticturkey.services.annotations.Optional;
 import it.uniroma2.art.semanticturkey.services.annotations.Read;
-import it.uniroma2.art.semanticturkey.services.annotations.RequestMethod;
 import it.uniroma2.art.semanticturkey.services.annotations.STService;
 import it.uniroma2.art.semanticturkey.services.annotations.STServiceOperation;
 import it.uniroma2.art.semanticturkey.services.support.QueryBuilder;
@@ -17,8 +14,10 @@ import it.uniroma2.art.semanticturkey.settings.core.SemanticTurkeyCoreSettingsMa
 import it.uniroma2.art.semanticturkey.user.UsersManager;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.vocabulary.RDFS;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
+import org.eclipse.rdf4j.queryrender.RenderUtils;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
 import org.eclipse.rdf4j.rio.helpers.NTriplesUtil;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -27,15 +26,28 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import static java.util.stream.Collectors.joining;
+
 @STService
 public class CustomTrees extends STServiceAdapter {
 
     @STServiceOperation
     @Read
     @PreAuthorize("@auth.isAuthorized('rdf(resource, taxonomy)', 'R')")
-    public Collection<AnnotatedValue<Resource>> getRoots(IRI cls, IRI childProp,
-            @Optional boolean invHierarchyDirection, @Optional boolean includeSubProp, @Optional boolean includeSubclasses) {
-        String hierarchyPath = getPathForHierarchy(childProp, invHierarchyDirection, includeSubProp);
+    public Collection<AnnotatedValue<Resource>> getRoots() throws STPropertyAccessException {
+
+        CorePUSettings puSettings = STPropertiesManager.getPUSettings(CorePUSettings.class, getProject(), UsersManager.getLoggedUser(), SemanticTurkeyCoreSettingsManager.class.getName());
+        CustomTreeSettings ctSettings = puSettings.customTree;
+
+        IRI type = ctSettings.type;
+        if (type == null) {
+            type = RDFS.RESOURCE;
+        }
+        IRI hierarchicalProperty = ctSettings.hierarchicalProperty;
+        boolean includeSubProp = ctSettings.includeSubProp;
+        boolean inverseHierarchyDirection = ctSettings.inverseHierarchyDirection;
+
+        String hierarchyPath = getPathForHierarchy(hierarchicalProperty, inverseHierarchyDirection, includeSubProp);
         // @formatter:off
         String query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" +
                 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
@@ -43,12 +55,7 @@ public class CustomTrees extends STServiceAdapter {
                 "PREFIX skos: <http://www.w3.org/2004/02/skos/core#> \n" +
                 "PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> \n" +
                 "SELECT ?resource ?attr_more " + generateNatureSPARQLSelectPart() + " \n" +
-                "WHERE { \n" +
-                "   ?resource rdf:type" + (includeSubclasses ? "/rdfs:subClassOf*" : "") + " ?cls . \n" +
-                "   FILTER NOT EXISTS { \n" +
-                "       ?parent (" + hierarchyPath + ") ?resource . \n" +
-                "       ?parent rdf:type"  + (includeSubclasses ? "/rdfs:subClassOf*" : "") + " ?cls . \n" +
-                "   } \n" +
+                "WHERE { \n" + getResourceWhere(ctSettings, hierarchyPath) +
                 "   BIND(EXISTS { \n" +
                 "       ?resource (" + hierarchyPath + ") ?child . \n" +
                 "   } as ?attr_more) \n" +
@@ -57,19 +64,56 @@ public class CustomTrees extends STServiceAdapter {
                 "GROUP BY ?resource ?attr_more";
         // @formatter:on
         QueryBuilder qb = createQueryBuilder(query);
-        qb.setBinding("cls", cls);
+        qb.setBinding("cls", type);
         qb.processRendering();
         qb.processQName();
         return qb.runQuery();
+    }
+
+    private String getResourceWhere(CustomTreeSettings ctSettings, String hierarchyPath) {
+        String rootCriteria = ctSettings.rootCriteria;
+        if (rootCriteria == null) {
+            rootCriteria = CustomTreeSettings.ROOT_CRITERIA_ALL;
+        }
+        List<IRI> roots = ctSettings.roots != null ? ctSettings.roots : new ArrayList<>();
+
+        String where = "";
+        if (rootCriteria.equals(CustomTreeSettings.ROOT_CRITERIA_STATIC)) {
+            //?resource is bound to statically defined values
+            where += "   VALUES(?resource) {" + roots.stream().map(iri -> "(" + RenderUtils.toSPARQL(iri) + ")").collect(joining()) + "}";
+        } else {
+            //?resource is bound to all those resources of the given type
+            where += "   ?resource rdf:type" + (ctSettings.includeSubtype ? "/rdfs:subClassOf*" : "") + " ?cls . \n" +
+                    "   FILTER NOT EXISTS { \n" +
+                    "       ?parent (" + hierarchyPath + ") ?resource . \n" +
+                    "       ?parent rdf:type" + (ctSettings.includeSubtype ? "/rdfs:subClassOf*" : "") + " ?cls . \n" +
+                    "   } \n";
+            //collect only resources with children
+            if (rootCriteria.equals(CustomTreeSettings.ROOT_CRITERIA_ONLY_WITH_CHILDREN)) {
+                where += "FILTER EXISTS { ?resource (" + hierarchyPath + ") ?child . } \n";
+            }
+        }
+        return where;
     }
 
 
     @STServiceOperation
     @Read
     @PreAuthorize("@auth.isAuthorized('rdf(resource, taxonomy)', 'R')")
-    public Collection<AnnotatedValue<Resource>> getChildrenResources(IRI cls, IRI parent, IRI childProp,
-            @Optional boolean invHierarchyDirection, @Optional boolean includeSubProp, @Optional boolean includeSubclasses) {
-        String hierarchyPath = getPathForHierarchy(childProp, invHierarchyDirection, includeSubProp);
+    public Collection<AnnotatedValue<Resource>> getChildrenResources(IRI resource) throws STPropertyAccessException {
+
+        CorePUSettings puSettings = STPropertiesManager.getPUSettings(CorePUSettings.class, getProject(), UsersManager.getLoggedUser(), SemanticTurkeyCoreSettingsManager.class.getName());
+        CustomTreeSettings ctSettings = puSettings.customTree;
+        IRI type = ctSettings.type;
+        if (type == null) {
+            type = RDFS.RESOURCE;
+        }
+        boolean includeSubtype = ctSettings.includeSubtype;
+        IRI hierarchicalProperty = ctSettings.hierarchicalProperty;
+        boolean includeSubProp = ctSettings.includeSubProp;
+        boolean inverseHierarchyDirection = ctSettings.inverseHierarchyDirection;
+
+        String hierarchyPath = getPathForHierarchy(hierarchicalProperty, inverseHierarchyDirection, includeSubProp);
         // @formatter:off
         String query = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> \n" +
                 "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> \n" +
@@ -78,7 +122,7 @@ public class CustomTrees extends STServiceAdapter {
                 "PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> \n" +
                 "SELECT ?resource ?attr_more " + generateNatureSPARQLSelectPart() + " \n" +
                 "WHERE { \n" +
-                "   ?resource rdf:type"  + (includeSubclasses ? "/rdfs:subClassOf*" : "") + " ?cls . \n" +
+                "   ?resource rdf:type"  + (includeSubtype ? "/rdfs:subClassOf*" : "") + " ?cls . \n" +
                 "   ?parent (" + hierarchyPath + ") ?resource . \n" +
                 "   BIND(EXISTS { \n" +
                 "       ?resource ("  + hierarchyPath + ") ?child . \n" +
@@ -88,8 +132,8 @@ public class CustomTrees extends STServiceAdapter {
                 "GROUP BY ?resource ?attr_more";
         // @formatter:on
         QueryBuilder qb = createQueryBuilder(query);
-        qb.setBinding("cls", cls);
-        qb.setBinding("parent", parent);
+        qb.setBinding("cls", type);
+        qb.setBinding("parent", resource);
         qb.processRendering();
         qb.processQName();
         return qb.runQuery();
@@ -117,27 +161,5 @@ public class CustomTrees extends STServiceAdapter {
         }
     }
 
-    @STServiceOperation(method = RequestMethod.POST)
-    @PreAuthorize("@auth.isAuthorized('pm(project,_)', 'U')")
-    public void storeCustomTreeProjectDefaultSettings(IRI type, IRI hierarchicalProperty)
-            throws STPropertyAccessException, STPropertyUpdateException {
-        CorePUSettings puSettings = STPropertiesManager.getPUSettingsProjectDefault(CorePUSettings.class, getProject(), SemanticTurkeyCoreSettingsManager.class.getName());
-        CustomTreeSettings ctSettings = new CustomTreeSettings();
-        ctSettings.type = type;
-        ctSettings.hierarchicalProperty = hierarchicalProperty;
-        puSettings.customTree = ctSettings;
-        STPropertiesManager.setPUSettingsProjectDefault(puSettings, getProject(), SemanticTurkeyCoreSettingsManager.class.getName(), true);
-    }
-
-    @STServiceOperation(method = RequestMethod.POST)
-    public void storeCustomTreeSettings(IRI type, IRI hierarchicalProperty)
-            throws STPropertyAccessException, STPropertyUpdateException {
-        CorePUSettings puSettings = STPropertiesManager.getPUSettings(CorePUSettings.class, getProject(), UsersManager.getLoggedUser(), SemanticTurkeyCoreSettingsManager.class.getName());
-        CustomTreeSettings ctSettings = new CustomTreeSettings();
-        ctSettings.type = type;
-        ctSettings.hierarchicalProperty = hierarchicalProperty;
-        puSettings.customTree = ctSettings;
-        STPropertiesManager.setPUSettings(puSettings, getProject(), UsersManager.getLoggedUser(), SemanticTurkeyCoreSettingsManager.class.getName());
-    }
 
 }
